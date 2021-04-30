911ad46da360c779f3298d7548c46171a35f9c2f
Erfan Asadi <50593999+Erfan-Asadi@users.noreply.github.com>
2021-04-30 11:06:00 +0430

    Upload main files from local

---
+++ style.css
@@ -0,0 +1,35 @@
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+.nav-line-bottom {
+  display: flex;
+list-style-type: none;
+  flex-flow: row nowrap;
+  height: 60px;
+  border: 2px solid salmon;
+  margin: 0 auto;
+  overflow: auto;
+}
+li {
+  min-width: 80px;
+  background-color: gainsboro;
+  margin: 0 4px;
+  font-weight: bold;
+}
+.about-tab-manup {
+  position: relative;
+  width: 400px;
+}
+.tns-controls button {
+  position: absolute;
+  top: 0;
+}
+.tns-controls button:nth-child(1) {
+  left: 0;
+}
+.tns-controls button:nth-child(2) {
+  right: 0;
+}

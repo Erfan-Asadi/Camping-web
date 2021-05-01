31048d298b3affd2391381aa79620ae43355979c
Erfan Asadi <asadi.love7981@gmail.com>
2021-05-01 18:59:05 +0430

    some change in script.js

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const hamburger = document.querySelector('.hamburger-btn');
 const navigation = document.querySelector('.landing-page nav');
 const landing_page = document.querySelector('.landing-page');
 const landing_page_clickable = document.querySelector('.landing-page[data-clickable="true"]');
+
 showNavigation =()=>{
     navigation.classList.add('expanded');
     landing_page.setAttribute('data-clickable','true');
 }
 hideNavigation =()=>{
+    alert('hi')
     navigation.classList.remove('expanded');
     landing_page.setAttribute('data-clickable','false');
 }
 
-landing_page_clickable.addEventListener('click',hideNavigation)
 hamburger.addEventListener('click',showNavigation)
+landing_page_clickable.addEventListener('click',hideNavigation)
 
   

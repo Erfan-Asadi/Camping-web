e51c626abf82a7942719128df5b78a4846e592db
Erfan Asadi <asadi.love7981@gmail.com>
2021-05-01 19:58:15 +0430

    add navigation collapse logic

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const hamburger = document.querySelector('.hamburger-btn');
 const navigation = document.querySelector('.landing-page nav');
 const landing_page = document.querySelector('.landing-page');
-const landing_page_clickable = document.querySelector('.landing-page[data-clickable="true"]');
+const landing_page_overlay = document.querySelector('.landing-page__overlay');
 
 showNavigation =()=>{
     navigation.classList.add('expanded');
-    landing_page.setAttribute('data-clickable','true');
+    landing_page_overlay.style.opacity = '1';
 }
 hideNavigation =()=>{
-    alert('hi')
     navigation.classList.remove('expanded');
-    landing_page.setAttribute('data-clickable','false');
+    landing_page_overlay.style.opacity = '0';
 }
 
 hamburger.addEventListener('click',showNavigation)
-landing_page_clickable.addEventListener('click',hideNavigation)
+landing_page_overlay.addEventListener('click',hideNavigation)
 
   

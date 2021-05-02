276467eb50a9caa3bfeac4398ca2acc1b6dcd338
Erfan Asadi <asadi.love7981@gmail.com>
2021-05-02 13:45:35 +0430

    add check_empty_input func for email btn

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
-    
+
     const hamburger = $('.hamburger-btn');
     const navigation = $('.landing-page nav');
     const landing_page_overlay = $('.landing-page__overlay');
-    
-    showNavigation =()=>{
+
+    showNavigation = () => {
         $(navigation).addClass('expanded');
-        $(landing_page_overlay).css({'opacity':'1'});
+        $(landing_page_overlay).css({ 'opacity': '1' });
     }
-    hideNavigation =()=>{
+    hideNavigation = () => {
         $(navigation).removeClass('expanded');
-        $(landing_page_overlay).css({'opacity':'0'});
+        $(landing_page_overlay).css({ 'opacity': '0' });
     }
-    
-    $(hamburger).on('click',showNavigation);
-    $(landing_page_overlay).on('click',hideNavigation);
+
+    $(hamburger).on('click', showNavigation);
+    $(landing_page_overlay).on('click', hideNavigation);
+
+
+    const email_input = $('#sendEmail-input');
+
+    check_input_empty = (e) => {
+        if ($(email_input).val() == '' || $(email_input).val().length == 0) {
+            alert('input is empty')
+            e.preventDefault();
+        }
+    }
+    $('form').on('submit', check_input_empty);
+
 });
 
-  

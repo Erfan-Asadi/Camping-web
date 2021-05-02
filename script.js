9e6534e5f9ffd901dc8e6acac19c02e226f71e59
Erfan Asadi <asadi.love7981@gmail.com>
2021-05-02 17:23:08 +0430

    add landing-page-btn animation

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ $(document).ready(function () {
 
     showNavigation = () => {
         $(navigation).addClass('expanded');
-        $(landing_page_overlay).css({ 'opacity': '1' });
+        $(landing_page_overlay).css({
+            'opacity': '1',
+            'z-index': '2'
+        });
     }
     hideNavigation = () => {
         $(navigation).removeClass('expanded');
-        $(landing_page_overlay).css({ 'opacity': '0' });
+        $(landing_page_overlay).css({
+            'opacity': '0',
+            'z-index': '0'
+        });
     }
 
     $(hamburger).on('click', showNavigation);
@@ -28,36 +34,37 @@ $(document).ready(function () {
     $('form').on('submit', check_input_empty);
 
     let heartIcon_container = document.querySelectorAll('.image-container span');
-  
+
     toggle_like_icon = (e) => {
         let mySpan = e.target;
         let mySpan_status = $(mySpan).hasClass('liked');
 
-        if ( mySpan_status == false) {
+        if (mySpan_status == false) {
             $(mySpan).addClass('liked');
-            $(mySpan).find('img').attr('src','image/heart-fill-icon.svg');
+            $(mySpan).find('img').attr('src', 'image/heart-fill-icon.svg');
         } else {
             $(mySpan).removeClass('liked')
-            $(mySpan).find('img').attr('src','image/heart-outline-icon.svg')
-           
+            $(mySpan).find('img').attr('src', 'image/heart-outline-icon.svg')
+
         }
     }
-    $(heartIcon_container).on('click',toggle_like_icon);
+    $(heartIcon_container).on('click', toggle_like_icon);
 
-    
+
     let arrowTop = $('.backToTop-btn');
 
-    arrowTop.on('click', function() {
+    arrowTop.on('click', function () {
+
         window.scrollTo(pageXOffset, 0);
         // after scrollTo, there will be a "scroll" event, so the arrow will hide automatically
-      });
-  
-      window.addEventListener('scroll', function() {
-          if (pageYOffset >= 400) {
-              $(arrowTop).css('opacity','1');
-        }  else {
-            $(arrowTop).css('opacity','0');
-          }
-      });
+    });
+
+    window.addEventListener('scroll', function () {
+        if (pageYOffset >= 400) {
+            $(arrowTop).css('opacity', '1');
+        } else {
+            $(arrowTop).css('opacity', '0');
+        }
+    });
 });
 

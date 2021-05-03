be05e3672ca9da5ad7d76024fef3d6abcc3ed51e
Erfan Asadi <asadi.love7981@gmail.com>
2021-05-03 22:20:54 +0430

    add email validator to input

--- script.js
+++ script.js
@@ -74,7 +74,44 @@ $(document).ready(function () {
         return `<button class="readMore-btn">${value}</button>`;
     }
 
-    
+
+    function check_email(input) {
+        // input should 'input type=email' that  passed to argument
+
+        let email_value = $(input).val();
+        const email_pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        email_validation_result = email_pattern.test(email_value);
+
+        if (email_validation_result == false) { // prevent add alert_tag again
+            $(input).next('.alert_form_message').remove();
+            $(input).after(alert_message('Email is not valid format'));  
+            $('.sendEmail-btn').attr('disabled','true')  
+        }
+        else if (email_validation_result == true) {
+            $(input).next('.alert_form_message').remove();
+            $('.sendEmail-btn').removeAttr('disabled')  
+        }
+    }
+    // show message bottom of input to alert user, when user 
+    function alert_message(alertText) {
+
+        let alert_tag = $(`<span class="alert_form_message">${alertText}</span>`);
+
+        $(alert_tag).css({
+            'position': 'absolute',
+            'left': '0',
+            'bottom': '-26px',
+            'font-size': '14px',
+            'color': '#dc3545',
+            'font-family':'Roboto'
+        })
+        return alert_tag;
+    }
+
+    // enable email checkers on those elements that hav this className
+    $(document.body).on('keyup', '.checkable-email', (e) => check_email(e.target));
+
+
     
     // این یک آرایه هستش که دوازده تا آبجکت توی خودش داره
     // هر آبجکت اطلاعاتی مثل 

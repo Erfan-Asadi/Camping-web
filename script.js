8ffec770adb5c17c1a40556e3a3a6140694f5275
Erfan Asadi <asadi.love7981@gmail.com>
2021-05-10 14:38:17 +0430

    remove persian comments

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 $(document).ready(function () {
     // variables
-    const hamburger = $('.hamburger-btn'); // همبرگر منوی نارنجی رنگی که تو سایزهای کوچیکتر بالا سمت راست صفحه میاد
-    const navigation = $('.landing-page nav'); // لیست لینکهای سمت چپ هدر صفحه
+    const hamburger = $('.hamburger-btn'); 
+    const navigation = $('.landing-page nav');
     
     const landing_page_overlay = $('.landing-page__overlay');
-     // زمانی که منوی همبرگری تو حالت موبایل باز میشه این همون کاور سیاه شفافی هستش که 
-    //  میاد روی صفحه و با کلیک روی این منو بسته میشه
    
     const email_input = $('#sendEmail-input'); 
 
     const arrowTop = $('.backToTop-btn');
 
     // functions 
-    showNavigation = () => { // وقتی روی همبرگر میزنیم این تابع باعث نمایش منو میشه( درحالت موبایل)
+    showNavigation = () => { 
         $(navigation).addClass('expanded');
         $(landing_page_overlay).css({
             'opacity': '1',
@@ -114,15 +112,6 @@ $(document).ready(function () {
     // enable email checkers on those elements that hav this className
     $(document.body).on('keyup', '.checkable-email', (e) => check_email(e.target));
 
-
-    
-    // این یک آرایه هستش که دوازده تا آبجکت توی خودش داره
-    // هر آبجکت اطلاعاتی مثل 
-    // imagePath : آدرس هر عکس مکان برای نمایش اون 
-    // title : عنوان مکان
-    // price: قیمت هر شب اسکان تو اون مکان
-    // detail: توضیحاتی که زیر عنوان مکان میاد
-    // button: نوشته ی دکمه ای که پایین هر آیتم مکان اومده 
     let destinations_information = [
         {
             imagePath: 'image/destination-1.png',
@@ -208,11 +197,8 @@ $(document).ready(function () {
             detail: 'Book unique camping experiences on over 234,000 campsites.',
             button: 'ReadMore'
         },
-    ]
+    ];
 
-    // آرایه ای از آبجکت ها که بالاتر گفتیم رو اینجا به این تابع میدم هر بخش اطلاعات آبجکت رو داخل تابع سازنده ی 
-    // خودش بریزه و کدهای اچ تی ام ال رو ایجاد کنه
-    // در آخر پس از پایان حلقه تمام اونهارو به صفحه اضافه کردم
     add_destination_items = (destination_array) => {
         $('.destinations-container').html('');
         for (let i = 0; i < destination_array.length; ++i) {
@@ -227,26 +213,25 @@ $(document).ready(function () {
             </section>        
             `;
 
-            $('.destinations-container').append(destination); // این نگهدارنده آیتم ایجاد شده رو هربار به فرزندان خودش اضافه میکنه
+            $('.destinations-container').append(destination); 
         }
     }
     add_destination_items(destinations_information);
 
-    // اون بوردر نارنجی رنگی که هر ثانیه دور یک آیتم مکان روشن میشه رو با این اینتروال ایجاد کردم
     let dest_index = 0;
     setInterval(() => {
 
-        $('.destination').eq(dest_index - 1).css('border-color', 'transparent'); // بوردر از آیتم قبلی گرفته میشه
-        $('.destination').eq(dest_index).css('border-color', '#e76f51'); // و به آیتم فعلی اضافه میشه
+        $('.destination').eq(dest_index - 1).css('border-color', 'transparent');
+        $('.destination').eq(dest_index).css('border-color', '#e76f51');
 
-        if (dest_index == $('.destination').length - 1) { // هروقت به آخرین آیتم رسیدیم شمارنده دوباره از اول آغاز به کار میکنه
+        if (dest_index == $('.destination').length - 1) { 
             dest_index = -1;
         }
         dest_index++;
 
     }, 1400);
 
-    show_cheap_destinations = (item)=> { // این تابع باعث میشه فقط مکان هایی که قیمت کمتر از 50 دارند نمایش داده بشن
+    show_cheap_destinations = (item)=> {
         $(item).siblings().removeClass('active')
         $(item).addClass('active')
         let cheap_list = destinations_information.filter((dest) => {
@@ -255,7 +240,7 @@ $(document).ready(function () {
            
         return cheap_list;
     }
-    show_normal_destinations = (item)=> { // مکان هایی که قیمت بین 50 تا 100 رو دارند فقط نمایش داده میشن
+    show_normal_destinations = (item)=> {
         $(item).siblings().removeClass('active')
         
         $(item).addClass('active')
@@ -265,7 +250,7 @@ $(document).ready(function () {
            
         return normal_list;
     }
-    show_expensive_destinations = (item)=> { // مکان های گرونتری که قیمت بالای صد دارند توی صفحه نشون داده میشن
+    show_expensive_destinations = (item)=> {
          $(item).siblings().removeClass('active')
          $(item).addClass('active')   
         let expensive_list = destinations_information.filter((dest) => {
@@ -274,7 +259,7 @@ $(document).ready(function () {
            
         return expensive_list;
     }
-    show_all_destinations = (item)=> { // مانند حالت اولیه تمام مکانهارو نشون میده روی مرورگر
+    show_all_destinations = (item)=> {
          $(item).siblings().removeClass('active')
          $(item).addClass('active')    
            
@@ -282,9 +267,7 @@ $(document).ready(function () {
     }
     
     
-    
-    // اینها هندلر های ما هستن یعنی برای اینکه تعیین کنیم هنگام کلیک یا اسکرول شدن عنصری، 
-    // چه اتفاقی بیوفته با این ایونت لیستنر ها اونارو مشخص میکنیم
+ 
     // event listeners
 
     // events that run after click on one of filter buttons
@@ -296,8 +279,6 @@ $(document).ready(function () {
 
 
     window.addEventListener('scroll', function () { 
-        // باعث نمایش داده شدن آیکون بازگشت به بالا میشه، زمانی که اندازه ی 400 واحد
-        // صفحه اسکرول شد اون آیکون نمایش داده میشه و با کلیک روش بر میگردیم به بالای صفحه
         if (pageYOffset >= 400) {
             $(arrowTop).css('opacity', '1');
         } else {
@@ -319,11 +300,11 @@ $(document).ready(function () {
 
     $('.landing-page-btn').on('click', () => window.scrollTo(0, 1000));
 
-    $('.destination').mouseenter(function () { // هنگامی که موس روی عکس مکان بیاد اندازه ی عکس کمی زوم میشه
+    $('.destination').mouseenter(function () {
         $(this).find('.image-container > img').css({ 'transform': 'scale(1.04)', 'cursor': 'pointer' })
 
     })
-    $('.destination').mouseleave(function () { // هنگامی که موس از روی عکس بره عکس مکان به حالت اولیه برمیگرده
+    $('.destination').mouseleave(function () {
         $(this).find('.image-container > img').css('transform', 'scale(1)')
 
     })
